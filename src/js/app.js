bab4a9aff57cf01d3aa2fbc7c0c82da20def7866
Denis Berezin <nocab@yandex.ru>
2019-05-07 20:46:43 +0300

    task2

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+var sum = 300;
+function balance (n, forms) {
+     return forms[(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2) ];
+
+}
+
+console.log('Ваш баланс: ' + sum + balance(sum, [" балл", " балла", " баллов"] ));
