3938b05cf18d977c8bb1ebf3b0888dd23d69eed4
Denis Berezin <nocab@yandex.ru>
2019-05-14 00:00:14 +0300

    lvl1.2

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,19 @@
-var sum = 300;
-function balance (n, forms) {
-     return forms[(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2) ];
+/*Первый вариант*/
+function showData (n) {
+    if (n%10==1 && n%100!=11) {
+        console.log(`Ваш баланс ${n} балл`);
+    } else if (n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20)){
+        console.log(`Ваш баланс ${n} балла`);
+    } else {
+        console.log(`Ваш баланс ${n} баллов`);
+    }
+}
+showData(10);
 
+/*Второй вариант*/
+/*balance = 10;
+function showData (n, forms) {
+    return forms[(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2) ];
 }
 
-console.log('Ваш баланс: ' + sum + balance(sum, [" балл3", " балла", " баллов"] ));
+console.log(`Ваш баланс: ${balance} ${showData(balance, [" балл", " балла", " баллов"])}`);*/
